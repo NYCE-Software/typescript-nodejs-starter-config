4b4db6a3caff009ccfa030180e3e841b544e2533
Bas Kager <bas@nyce.dev>
2022-12-01 15:40:16 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # config
+
 Common configuration files used across projects
+
+## Usage
+
+### package.json
+
+> [ESLINT forces you to use a prefix on your WHOLE DAMN NPM PACKAGE](https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules) (seriously?!).
+>
+> We serve multiple configuration files in this package so prefixing it with `eslint-config-` would be confusing. The ESlint configuration can be imported by using an absolute path.
+
+````json
+{
+  "prettier": "@nyce/config/prettier",
+  "eslintConfig": {
+    "extends": "./node_modules/@nyce/config/eslint"
+  }
+}
+```software
+````
---
+++ eslint.js
@@ -0,0 +1,58 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        // TypeScript
+        "@typescript-eslint",
+        // Security
+        "no-secrets",
+        "xss",
+        // Optimization
+        "regexp",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest", // Allows the use of modern ECMAScript features
+        sourceType: "module", // Allows for the use of imports
+        project: "tsconfig.json",
+    },
+    extends: [
+        "eslint:recommended",
+        // TypeScript
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        // Code style
+        "prettier",
+        // Security
+        "plugin:no-unsanitized/DOM",
+        "plugin:pii/recommended",
+        // Optimization
+        "plugin:regexp/recommended",
+    ],
+    env: {
+        node: true, // Enable Node.js global variables
+    },
+    rules: {
+        // no-dob rule has a bug which crashes on loops, turned off.
+        "pii/no-dob": "off",
+        // Debugging
+        "no-console": "warn",
+        // Type
+        "@typescript-eslint/no-inferrable-types": [
+            "off",
+            {
+                ignoreParameters: true,
+            },
+        ],
+        "@typescript-eslint/typedef": [
+            "error",
+            {
+                arrowParameter: true,
+                variableDeclaration: true,
+            },
+        ],
+        // Security
+        "no-secrets/no-secrets": "error",
+        "xss/no-mixed-html": "error",
+        "xss/no-location-href-assign": "error",
+    },
+};
